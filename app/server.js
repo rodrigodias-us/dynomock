2b50f22e445a48bf51e572f9f7f8a396451ccd21
Rodrigo Dias <rodrigogd@ciandt.com>
2016-12-05 15:27:33 -0200

    Adiciona gravação de cache por arquivos

--- app/server.js
+++ app/server.js
@@ -1,15 +1,15 @@
-const NodeCache = require( "node-cache" );
-const myCache = new NodeCache();
-
+var fs = require('fs');
 var querystring = require('querystring');
 var express = require('express');
 var http = require('http');
 var utils = require('./utils');
 
+var folderCache = 'mock_files';
 
 function Server(host, port){
 	var app = express();
 	var bodyParser = require('body-parser');
+	
 	app.use(bodyParser.urlencoded({extended: false}));
 	
 	app.set('views', './views');
@@ -17,31 +17,44 @@ function Server(host, port){
 
 	app.get('/mock/:mock_id/delete', function(request, response){
 		var mockId = request.params.mock_id;
-		myCache.del(mockId, function( err, count ){
+		var pathMock = folderCache + '/' + mockId + '.json';
+		
+		fs.unlink(pathMock, function(err){
+			if(err) return console.log(err);
+			console.log('Mock: ' + mockId + ' deleted successfully');
 			response.render('mock-deleted', {id: mockId});
 		});
 	});
 	
 	app.get('/mock/:mock_id', function(request, response){
 		var mockId = request.params.mock_id;
-		var cached = myCache.get(mockId);
-		if (cached){
-			response.render('mock', { id: mockId, mock: cached, body: JSON.stringify(cached.body, null, 2)});
-		} else {
-			response.render('mock', { id: mockId, mock: cached });
-		}
+		var pathMock = folderCache + '/' + mockId + '.json';
+		
+		fs.readFile(pathMock, 'utf8', (err, data) => {
+			if (!err) {
+				var cached = JSON.parse(data);
+			
+				if (cached){
+					response.render('mock', { id: mockId, mock: cached, body: JSON.stringify(cached, null, 2)});
+				} else {
+					response.render('mock', { id: mockId, mock: cached });
+				}
+			}
+		});
 	});
 	
 	app.post('/mock/:mock_id', function(request, response){
 		var mockId = request.params.mock_id;
-		var cached = myCache.get(mockId);
-		cached.body = JSON.parse(request.body.json);
-		myCache.set(mockId, cached, function( err, success ){
-			if( !err && success ){
-				console.log( "SAVE CACHE -> " + success );
-				console.log( "OBJECT -> " + cached );
-				response.render('mock', { id: mockId, mock: cached, body: JSON.stringify(cached.body, null, 2)});
-			}
+		var pathMock = folderCache + '/' + mockId + '.json';
+		
+		var cache = JSON.parse(request.body.json);
+		
+		fs.writeFile(pathMock, JSON.stringify(cache, null, 2), function(err) {
+		    if(err) {
+		        return console.log(err);
+		    }
+			response.render('mock', { id: mockId, mock: cache, body: JSON.stringify(cache, null, 2)});
+			console.log( "SAVE CACHE FILE -> " + mockId );
 		});
 	});
 	
@@ -59,63 +72,70 @@ function Server(host, port){
 		}).on('data', function(chunk) {
 			body += chunk;
 		}).on('end', function() {
-			var keyCache = utils.makeMd5(body);
-			var cached = myCache.get(keyCache);
-			if ( cached == undefined ){
-				
-				var options = {
-					hostname: host,
-					path: path,
-					port: port,
-					method: method,
-					headers: headers
-				}
-				
-				var dimas = http.request(options, (res) => {
-					var body = '';
-					
-					res.on('data', (chunk) => {
-						body += chunk;
-					}).on('end', () => {
-						body = JSON.parse(body);
-						
-						console.log("EITAAA: " + body.statusCode);
-
-						cache = {body: body, headers: res.headers, statusCode: res.statusCode};
-						myCache.set(keyCache, cache, function( err, success ){
-							if( !err && success ){
-								console.log( "SAVE CACHE -> " + success );
-								console.log( "BODY -> " + body );
-							}
-						});
-						
-						body.mockID = keyCache;
-						response.set(res.headers)
-						.status(res.statusCode)
-						.json(body);
-					});
-				}).on('error', (e) => {
-					response.json({error: "Não foi encontrato o Dimas"});
-				});
-				
-				console.log("CHAMA DIMAS: " + body);
-				dimas.write(body);
-				
-			} else {
-				cached.body.mockID = keyCache;
-				
-				response
-				.set(cached.headers)
-				.status(cached.statusCode)
-				.json(cached.body);
+			var mockId = utils.makeMd5(body);
+			var pathMock = folderCache + '/' + mockId + '.json';
+			
+			if (!fs.existsSync(folderCache)){
+			    fs.mkdirSync(folderCache);
 			}
-	
+			
+			fs.readFile(pathMock, 'utf8', (err, data) => {
+				if (!err) {
+					console.log( "SEND CACHE FILE -> " + mockId );
+					var cached = JSON.parse(data);
+					cached.body.mockID = mockId;
+
+					response
+					.set(cached.headers)
+					.status(200)
+					.json(cached.body);
+				} else {
+					var options = {
+						hostname: host,
+						path: path,
+						port: port,
+						method: method,
+						headers: headers
+					}
+				
+					var dimas = http.request(options, (res) => {
+						var body = '';
+					
+						res.on('data', (chunk) => {
+							body += chunk;
+						}).on('end', () => {
+							body = JSON.parse(body);
+
+							cache = {body: body, headers: res.headers};
+							
+							fs.writeFile(pathMock, JSON.stringify(cache, null, 2), function(err) {
+							    if(err) {
+							        return console.log(err);
+							    }
+
+								console.log( "SAVE CACHE FILE -> " + mockId );
+							}); 
+
+							body.mockID = mockId;
+							response.set(res.headers)
+							.status(200)
+							.json(body);
+						});
+					}).on('error', (e) => {
+						response.json({error: "Não foi encontrato o Dimas"});
+					});
+				
+					console.log("ACCESS DIMAS!");
+					dimas.write(body);
+				}
+			});
 		});
 	});
 	
 	this.start = function(){
 		//create node.js http server and listen on port
 		console.log("START SERVER TO "+ host+ ":" + port);
+		console.log("LISTEN : 3000");
 		http.createServer(app).listen(3000);
 	};
 	
